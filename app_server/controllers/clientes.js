62d90f0ffc1335f6fe21a48da3a8d98d5b78c478
Javodg <javodg@gmail.com>
2016-03-06 14:53:12 -0600

    Formulario de nuevo cliente, en progreso

---
+++ app_server/controllers/clientes.js
@@ -0,0 +1,12 @@
+module.exports.lista = function (req, res) {
+	res.render('lista_clientes', { title:'Lista de Clientes' });
+};
+module.exports.nuevo = function (req, res) {
+	res.render('nuevocliente', { title:'Lista de Clientes' });
+};
+module.exports.cliente = function (req, res) {
+	res.render('lista_clientes', { title:'Lista de Clientes' });
+};
+module.exports.clientes = function (req, res) {
+	res.render('lista_clientes', { title:'Lista de Clientes' });
+};
